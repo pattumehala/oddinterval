85fbf850f7664e7d1a94f84629f952c28625f271
pattumehala <35220817+pattumehala@users.noreply.github.com>
2018-02-02 16:11:06 +0530

    oddinterval.C
    odd numbers between two interval

---
+++ oddinterval.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int low,high,flag,i;
+printf("Enter the two numbers(interval):",low,high);
+scanf("%d%d",&low,&high);
+printf("the odd number between %d and %d is:",low,high);
+for(i=low;i<=high;i++)
+{
+flag=i%2;
+if(flag!=0)
+{
+printf("%d",i);
+}
+}
+return 0;
+}
